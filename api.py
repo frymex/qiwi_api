33ebd11cae47025f0c0c8c2b39bc90f13c0e532f
frymex <t_frymex@protonmail.com>
2022-07-29 00:31:11 +0300

    z

--- api.py
+++ api.py
@@ -1,17 +1,15 @@
 import asyncio
+import base64
 from typing import Union, Any
 
 import requests
-import base64
-import os
-
-from types_qiwi.types import *
 from requests.cookies import RequestsCookieJar
-
 from twocaptcha import TwoCaptcha
 
+from types_qiwi.types import *
 
-class z_py:
+
+class SaveDict:
     phone, password = None, None
     web_qw = None
     ru_captcha_key = None
@@ -22,7 +20,7 @@ def _gen_headers():
         'authority': 'edge.qiwi.com',
         'accept': 'application/json',
         'accept-language': 'ru',
-        'authorization': f'TokenHeadV2 {z_py.web_qw}',
+        'authorization': f'TokenHeadV2 {SaveDict.web_qw}',
         'client-software': 'WEB v5.0.0',
         'content-type': 'application/json',
         'dnt': '1',
@@ -34,7 +32,8 @@ def _gen_headers():
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/103.0.0.0 Safari/537.36',
     }
     return headers_sys
 
@@ -49,7 +48,10 @@ def _gen_reg_platform():
         'token-tail': 'e1f6ab31b89d849d',
         'JSESSIONID': '64C7FB517FA6C2606E9AE0D2264E0843.node-s3242',
         'test.for.third.party.cookie': 'yes',
-        'auth_ukafokfuabbuzdckyiwlunsh': 'MDI1fF98X3xcMQhGAm0EaV4Lb30Pf38zAkVkTgo2aHwgDHwUUXERVzw7XwJJZHsMcQBXRltcdEtGDydDfkYCLkdQZVB7Y1BFWDBAEwc6VHdCVjp8WDR+YAJJKkVCYm9kfV57RwN0Qg==',
+        'auth_ukafokfuabbuzdckyiwlunsh': 'MDI1fF98X3xcMQhGAm0EaV4Lb30Pf38zAkVkTgo'
+                                         '2aHwgDHwUUXERVzw7XwJJZ'
+                                         'HsMcQBXRltcdEtGDydDfkYCLkdQZVB7Y1BFWDBAEwc'
+                                         '6VHdCVjp8WDR+YAJJKkVCYm9kfV57RwN0Qg==',
     }
 
     headers = {
@@ -65,7 +67,8 @@ def _gen_reg_platform():
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/103.0.0.0 Safari/537.36',
     }
     return cookies, headers
 
@@ -88,28 +91,27 @@ class Qiwi:
                       '0}/simple-identification-applications '
 
     def __init__(self, phone_number: [str, int, None], password: [str, None], qiwi_v2=None):
-        z_py.password, z_py.phone = password, phone_number
+        SaveDict.password, SaveDict.phone = password, phone_number
         self.cookies = None
         self.widgetAliasCode = None
 
-        self.captcha = TwoCaptcha(z_py.ru_captcha_key, server='rucaptcha.com')
+        self.captcha = TwoCaptcha(SaveDict.ru_captcha_key, server='rucaptcha.com')
 
         if not qiwi_v2:
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
             asyncio.set_event_loop(asyncio.new_event_loop())
             asyncio.run(self.save_account())
         else:
-            z_py.web_qw = qiwi_v2.get('web-qw')
+            SaveDict.web_qw = qiwi_v2.get('web-qw')
             x = RequestsCookieJar()
             cookies: dict = qiwi_v2.get('cookies')
             for ix in cookies:
                 x.set(ix, cookies.get(ix))
             self.cookies = x
 
-    async def save_account(self):
+    async def save_account(self) -> bool:
         with requests.Session() as c:
-            if 'session.json' in os.listdir('./'):
-                return
+
             cookies = {
                 'landing_count': '1',
                 'user_info': '1',
@@ -117,7 +119,9 @@ class Qiwi:
                 'test.for.third.party.cookie': 'yes',
                 'token-tail': 'a03100d182a041ce',
                 'landing_name': 'qiwitoday',
-                'auth_ukafokfuabbuzdckyiwlunsh': 'MDAyfF98X3wAV2IBSS17fmwlRlVxVVl1CXVkAFIvI38VYVxmL1l+C1AEAFd+e09sTEhHV39aZ3J+S20QC14CLGQRbioCXmAKWlN8A04seGdyJRNQdR4LewxxLAJPenNnS2xYZHdedQ==',
+                'auth_ukafokfuabbuzdckyiwlunsh': 'MDAyfF98X3wAV2IBSS17fmwlRlVxVVl1CXVkAFIvI38VYVxmL1l+C1AEAFd+e09sTEh'
+                                                 'HV39aZ3J'
+                                                 '+S20QC14CLGQRbioCXmAKWlN8A04seGdyJRNQdR4LewxxLAJPenNnS2xYZHdedQ==',
             }
 
             headers = {
@@ -134,7 +138,8 @@ class Qiwi:
                 'sec-fetch-dest': 'empty',
                 'sec-fetch-mode': 'cors',
                 'sec-fetch-site': 'same-origin',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/103.0.0.0 Safari/537.36',
             }
 
             data = {
@@ -143,23 +148,22 @@ class Qiwi:
                 'client_id': 'web-qw',
                 'client_secret': 'P0CGsaulvHy9',
                 'anonymous_token_head': '8c482e59ae74e7e2',
-                'username': f'+{str(z_py.phone)}',
-                'password': f'{z_py.password}',
+                'username': f'+{str(SaveDict.phone)}',
+                'password': f'{SaveDict.password}',
             }
 
             response = c.post('https://qiwi.com/oauth/token', cookies=cookies, headers=headers, data=data)
-            json_: dict = response.json()
-            if json_.get('error', None):
-                err_text = json_.get('user_message', None)
+            result: dict = response.json()
+            if result.get('error', None):
+                err_text = result.get('user_message', None)
                 exit(f'[!] Qiwi error: {err_text}')
 
             cookies = response.cookies
             self.cookies = cookies
 
-            token1 = json_.get('access_token', None)
-            token2 = json_.get('refresh_token', None)
+            token1 = result.get('access_token', None)
 
-            if not token1 and not token2:
+            if not token1:
                 return False
 
             qq = f'web-qw:{token1}'
@@ -168,74 +172,75 @@ class Qiwi:
             base64_bytes = base64.b64encode(message_bytes)
             base64_message = base64_bytes.decode('ascii')
 
-            z_py.web_qw = base64_message
+            SaveDict.web_qw = base64_message
 
     def get_balance(self) -> Union[Accounts, int]:
 
-        response_sys = requests.get(Qiwi.accounts_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies)
+        system_response = requests.get(Qiwi.accounts_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies)
 
-        if response_sys.ok:
+        if system_response.ok:
 
-            return Accounts(response_sys.json())
+            return Accounts(system_response.json())
 
         else:
-            return response_sys.status_code
+            return system_response.status_code
 
     def get_identifications(self) -> Identifications:
 
-        response_sys = requests.get(Qiwi.identifications_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies)
+        system_response = requests.get(Qiwi.identifications_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies)
 
-        return Identifications(response_sys.json())
+        return Identifications(system_response.json())
 
     def get_available_access_methods(self):
 
-        response_sys = requests.get(Qiwi.access_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies)
+        system_response = requests.get(Qiwi.access_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies)
 
-        return Access(response_sys.json())
+        return Access(system_response.json())
 
     def create_p2p_keys(self, pair_name: str) -> NewPairP2p:
+
         json_data = {
             'keysPairName': f'{pair_name}',
         }
 
-        response_sys = requests.post(Qiwi.p2p_create_keys_obj,
-                                     headers=_gen_headers(), cookies=self.cookies, json=json_data)
+        system_response = requests.post(Qiwi.p2p_create_keys_obj,
+                                        headers=_gen_headers(), cookies=self.cookies, json=json_data)
 
-        return NewPairP2p(response_sys.json())
+        return NewPairP2p(system_response.json())
 
     def get_my_p2p_keys(self) -> MyP2P_Keys:
 
-        response_sys = requests.get(Qiwi.p2p_key_obj,
-                                    headers=_gen_headers(), cookies=self.cookies)
+        system_response = requests.get(Qiwi.p2p_key_obj,
+                                       headers=_gen_headers(), cookies=self.cookies)
 
-        return MyP2P_Keys(response_sys.json())
+        return MyP2P_Keys(system_response.json())
 
-    def block_p2p_key(self, public_key: str):
+    def block_p2p_key(self, public_key: str) -> bool:
 
         json_data = {
             'publicKey': f'{public_key}',
         }
 
-        response_sys = requests.post(Qiwi.p2p_block_key_obj,
-                                     headers=_gen_headers(), cookies=self.cookies, json=json_data)
-        if response_sys.status_code == 500:
+        system_response = requests.post(Qiwi.p2p_block_key_obj,
+                                        headers=_gen_headers(), cookies=self.cookies, json=json_data)
+        if system_response.status_code == 500:
             return False
         else:
             return True
 
     def _gen_public_key_p2p(self) -> Union[bool, Any]:
 
-        response_sys = requests.post('https://edge.qiwi.com/widgets-api/api/p2p/protected/generate-public-key',
-                                     headers=_gen_headers(), cookies=self.cookies)
+        system_response = requests.post('https://edge.qiwi.com/widgets-api/api/p2p/protected/generate-public-key',
+                                        headers=_gen_headers(), cookies=self.cookies)
 
-        if response_sys.status_code == 500:
+        if system_response.status_code == 500:
             return False
         else:
-            self.widgetAliasCode = response_sys.json().get('widgetAliasCode')
-            return response_sys.json()
+            self.widgetAliasCode = system_response.json().get('widgetAliasCode')
+            return system_response.json()
 
     def create_invoice(self, amount: int, comment: str = 'Qiwi Client By @cazqev') -> Union[bool, Any]:
 
@@ -262,29 +267,29 @@ class Qiwi:
             'public_key': q.get('publicKey'),
         }
 
-        response_sys = requests.post(Qiwi.p2p_create_invoice_obj,
-                                     headers=_gen_headers(), cookies=self.cookies, json=json_data)
+        system_response = requests.post(Qiwi.p2p_create_invoice_obj,
+                                        headers=_gen_headers(), cookies=self.cookies, json=json_data)
 
-        if response_sys.status_code == 500:
+        if system_response.status_code == 500:
             return False
         else:
-            return response_sys.json().get('invoice_uid')
+            return system_response.json().get('invoice_uid')
 
     @property
     def get_my_input_form_url(self) -> str:
         return 'https://my.qiwi.com/{0}'.format(self.widgetAliasCode)
 
     def get_history(self, rows: int = 10) -> dict:
-        response_sys = requests.get(Qiwi.history_obj.format(z_py.phone, rows),
-                                    headers=_gen_headers(), cookies=self.cookies)
-        if response_sys.ok:
-            return response_sys.json()
+        system_response = requests.get(Qiwi.history_obj.format(SaveDict.phone, rows),
+                                       headers=_gen_headers(), cookies=self.cookies)
+        if system_response.ok:
+            return system_response.json()
         else:
-            exit(response_sys.json())
+            exit(system_response.json())
 
     @property
     def get_my_token_v2(self):
-        return z_py.web_qw
+        return SaveDict.web_qw
 
     @property
     def get_my_session(self):
@@ -325,10 +330,10 @@ class Qiwi:
 
     def get_nickname(self) -> Nickname:
 
-        response_sys = requests.get(Qiwi.nickname_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies)
+        system_response = requests.get(Qiwi.nickname_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies)
 
-        return Nickname(response_sys.json())
+        return Nickname(system_response.json())
 
     def gen_nickname(self) -> Nickname:
 
@@ -336,10 +341,10 @@ class Qiwi:
             'type': 'GENERATE',
         }
 
-        response_sys = requests.put(Qiwi.nickname_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies, json=json_data)
+        system_response = requests.put(Qiwi.nickname_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies, json=json_data)
 
-        return Nickname(response_sys.json())
+        return Nickname(system_response.json())
 
     def create_nickname(self, nickname: str) -> Nickname:
 
@@ -348,39 +353,40 @@ class Qiwi:
             'nickname': nickname
         }
 
-        response_sys = requests.put(Qiwi.nickname_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies, json=json_data)
+        system_response = requests.put(Qiwi.nickname_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies, json=json_data)
 
-        return Nickname(response_sys.json())
+        return Nickname(system_response.json())
 
     def get_email(self):
-        response_sys = requests.get(Qiwi.email_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies)
+        system_response = requests.get(Qiwi.email_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies)
 
-        return response_sys.json()
+        return system_response.json()
 
     def add_email(self, email: str, change_password_via_email: bool = True, get_promos_to_email: bool = False):
 
-        ix_json = {"email": email,
-                   "flags": {"USE_FOR_PROMO": get_promos_to_email, "USE_FOR_SECURITY": change_password_via_email},
-                   "operationId": "602526688283"}
+        json_data = {"email": email,
+                     "flags": {"USE_FOR_PROMO": get_promos_to_email, "USE_FOR_SECURITY": change_password_via_email},
+                     "operationId": "602526688283"}
 
-        response_sys = requests.put(Qiwi.email_obj.format(z_py.phone),
-                                    headers=_gen_headers(), cookies=self.cookies, json=ix_json)
+        system_response = requests.put(Qiwi.email_obj.format(SaveDict.phone),
+                                       headers=_gen_headers(), cookies=self.cookies, json=json_data)
 
-        return response_sys.json()
+        return system_response.json()
 
     def confirm_email(self, code: [str, int]):
 
-        ix_json = {'code': code}
-        response_sys = requests.post(Qiwi.confirm_email_obj.format(z_py.phone),
-                                     headers=_gen_headers(), cookies=self.cookies, json=ix_json)
+        json_data = {'code': code}
+        system_response = requests.post(Qiwi.confirm_email_obj.format(SaveDict.phone),
+                                        headers=_gen_headers(), cookies=self.cookies, json=json_data)
 
-        return response_sys.json().get('status')
+        return system_response.json().get('status')
 
     def create_account(self, phone_number: [str, int], logs: bool = False) -> Union[bool, Any]:
         if logs:
             print('[! CREATING !] Решение капчи')
+
         result = self.captcha.recaptcha(sitekey='6LfjX_4SAAAAAFfINkDklY_r2Q5BRiEqmLjs4UAC',
                                         url='https://api.qiwi.com/register/form.action?ref=newsite_index_1',
                                         version='v2')
@@ -395,10 +401,11 @@ class Qiwi:
         session = requests.Session()
         if logs:
             print('[! CREATING !] Авторизация в Qiwi')
-        response1 = session.post('https://api.qiwi.com/oauth/authorize', cookies=_gen_reg_platform()[0],
-                                 headers=_gen_reg_platform()[1], data=data)
-        if response1.ok:
-            return response1.json().get('confirmation_id')
+
+        get_confirmation_id = session.post('https://api.qiwi.com/oauth/authorize', cookies=_gen_reg_platform()[0],
+                                           headers=_gen_reg_platform()[1], data=data)
+        if get_confirmation_id.ok:
+            return get_confirmation_id.json().get('confirmation_id')
         else:
             return False
 
@@ -431,7 +438,10 @@ class Qiwi:
                 'token-tail': '912144989576f070',
                 'JSESSIONID': 'C14A7E7A3CB5B682B3E793D02D89E28A.node-s3253',
                 'ref': 'newsite_index_1',
-                'auth_ukafokfuabbuzdckyiwlunsh': 'MDA1fF98X3wGNVJ4MVgPYGNYDwcAG0AREFRVQFALUE4KKHc1FnQWew4YcGhoB2Z/CXRaZVJ+aEpqPVhxKhANFEkXFEdlWgBjA2MadmBUWH58VFwDUgJDREMAGh0YX1dWV3pxZkR3SA==',
+                'auth_ukafokfuabbuzdckyiwlunsh': 'MDA1fF98X3wGNVJ4MVgPYGNYDwcAG0AREFRVQFALUE4KKHc1FnQWew4YcGhoB2Z'
+                                                 '/CXRaZVJ'
+                                                 '+aEpqPVhxKhANFEkXFEdlWgBjA2MadmBUWH5'
+                                                 '8VFwDUgJDREMAGh0YX1dWV3pxZkR3SA==',
             }
 
             headers = {
@@ -447,7 +457,8 @@ class Qiwi:
                 'sec-fetch-dest': 'empty',
                 'sec-fetch-mode': 'cors',
                 'sec-fetch-site': 'same-origin',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/103.0.0.0 Safari/537.36',
             }
 
             data = {
@@ -461,8 +472,8 @@ class Qiwi:
             response = session.post('https://api.qiwi.com/oauth/authorize', cookies=cookies, headers=headers, data=data)
 
             if response.ok:
-                z_py.phone = str(phone)
-                z_py.password = password
+                SaveDict.phone = str(phone)
+                SaveDict.password = password
 
                 self.save_account()
 
@@ -474,13 +485,3 @@ class Qiwi:
         else:
             js_: dict = response1.json()
             return f'Ошибка: {js_.get("error_description", "no info")}'
-
-    def update_verification(self, birth_date: str, country: str = 'RU',
-                            first_name: str = '', last_name: str = '',
-                            middle_name: str = '', passport: str = '',
-                            ):
-        pass
-
-    def create_api_key(self):
-        pass
-
